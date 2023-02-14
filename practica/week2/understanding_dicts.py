edd3a12e7cc0ce66757623a24bbe9062301f082f
Jdownkast <1067055@student.hr.nl>
2023-02-14 16:02:34 +0100

    14/2

---
+++ practica/week2/understanding_dicts.py
@@ -0,0 +1,10 @@
+team1 = {"Name": "RedBull", "Color": "Blue"}
+team2 = {"Name": "Mercedes", "Color": "Green"}
+team3 = {"Name": "Ferrari", "Color": "Red"}
+teams = {
+    "Team1": team1,
+    "Team2": {"Name": "Mercedes", "Color": "Green"},
+    "Team3": {"Name": "Ferrari", "Color": "Red"},
+}
+
+print(teams)
